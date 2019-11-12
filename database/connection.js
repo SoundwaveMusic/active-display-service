2a94a017a994041f05581fc438f5b10fd5f7f75c
Thomas Johnson <thomasij813@gmail.com>
2019-11-11 19:22:41 -0800

    use config file for db connection

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,13 @@
 const { Pool } = require('pg');
+const secret = require('./dbConfig.js');
 
 const pool = new Pool({
-  host: 'localhost',
-  database: 'jastdance',
-  port: 5432
+  host: secret.host || 'localhost',
+  database: secret.database || 'postgres',
+  port: secret.port || 5432,
+  user: secret.user || 'postgres',
+  password: secret.password || 'postgres',
+  max: 10
 });
 
 pool.on('error', (err, client) => {
@@ -14,50 +18,3 @@ pool.on('error', (err, client) => {
 pool.connect();
 
 module.exports = pool;
-// const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://127.0.0.1/display');
-
-// const database = mongoose.connection;
-
-// database.on('error', console.error.bind(console, 'connection error:'));
-// database.once('open', () => {});
-
-// const songSchema = new mongoose.Schema({
-//   id: {
-//     type: Number,
-//     unique: true,
-//   },
-//   artist: String,
-//   title: String,
-//   artwork: String, // ← This is a URL String
-//   postingDate: Date,
-//   tag: String,
-//   waveform: String, // ← This is a URL String
-//   lengthInSeconds: Number,
-//   currentTimestampInSeconds: Number,
-//   comments: [
-//     {
-//       username: String,
-//       avatar: String, // ← This is a URL String
-//       text: String,
-//       timestampInSeconds: Number,
-//     },
-//   ],
-// });
-
-// const Song = mongoose.model('Song', songSchema);
-
-// const findOneSong = (songId, callback) => {
-//   Song.find({ id: songId }).exec((_, song) => callback(song));
-// };
-// const updateOneSong = (songId, updateKeyAndValue, callback) => {
-//   Song.update({ id: songId }, updateKeyAndValue).exec((_, song) => callback(song));
-// };
-
-// module.exports = {
-//   database,
-//   Song,
-//   findOneSong,
-//   updateOneSong,
-// };
